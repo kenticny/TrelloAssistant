03683334867e82af347c68cffa126a073fbbbb99
luyun <luyun@goyoo.com>
2014-11-28 20:23:09 +0800

    integrate trello oauth

--- assets/javascripts/background.js
+++ assets/javascripts/background.js
@@ -1,14 +1,16 @@
+// ------------------ TOOL ------------------------
+
 /**
- * send GET request
+ * 发送GET请求 (send GET request with sign)
  * @param  {[string]}   url      [url]
  * @param  {Function} callback 
  */
 function getRequest(url, callback) {
-  $.get(url, callback);
+  oa.sendSignedRequest(url, callback, {method: "GET"});
 }
 
 /**
- * send POST request
+ * 发送POST请求 (send POST request with sign)
  * @param  {[string]}   url      [url]
  * @param  {[object]}   [params]   Optional[parameters with request]
  * @param  {Function} callback 
@@ -17,46 +19,95 @@ function postRequest(url, params, callback) {
   if(arguments.length == 2) {
     callback = params; params = null;
   }
-  $.post(url, params || {}, callback);
+  oa.sendSignedRequest(url, callback, {
+      method: "POST", 
+      parameters: params || {} 
+  });
 }
 
 /**
- * get Current User Trello Boards
- * @param  {Function} callback
+ * OAuth
+ * @param  {[string]} key    [key]
+ * @param  {[string]} secret [secret]
+ * @param  {Function} callback 
  */
-function getBoards(callback) {
-  getRequest(config.urls.boards, callback);
+function oauth(key, secret, callback) {
+  var oauth = ChromeExOAuth.initBackgroundPage({
+    "request_url" : "https://trello.com/1/OAuthGetRequestToken",
+    "authorize_url" : "https://trello.com/1/OAuthAuthorizeToken",
+    "access_url" : "https://trello.com/1/OAuthGetAccessToken",
+    "consumer_key" : key,
+    "consumer_secret" : secret,
+    //"scope" : "https://api.trello.com/1/",
+    "app_name" : "Trello Assistant"
+  });
+  return oauth;
 }
 
 /**
- * get current user infomation
+ * 生成带参数的URL (replace url with params)
+ * @param  {[string]} url    [url template]
+ * @param  {[object]} params [params object, param name -> param value]
+ * @return {[string]}        [complete url]
+ */
+function renderUrl(url, params) {
+  for(var p in params) {
+    url = url.replace("{" + p + "}", params[p]);
+  }
+  return url;
+}
+
+
+// -------------- Trello API --------------------
+
+/**
+ * 获取当前用户信息 (get current user infomation)
  * @param  {Function} callback 
  */
 function getCurrentUser(callback) {
-  getRequest(config.urls.user + '?notifications=all', callback);
+  getRequest(config.basic.host + config.basic.version + config.api.member.me, callback);
 }
 
 /**
- * get board content by board id
- * @param  {[string]}   id       [board id]
- * @param  {Function} callback 
- */
-function getBoardById(id, callback) {
-  getRequest(config.urls.board + '/' + id + '?lists=all&cards=visible', callback);
-}
-
-/**
- * get the number of current user unread notifications
+ * 获取用户的提醒消息
+ * get the number of current user notifications
  * @param  {Function} callback
  */
-function getUnreadNoticeNumber(callback) {
-  getCurrentUser(function(result) {
-    callback(result.notifications.filter(function(n) {return n.unread;}).length);
-  })
+function getNotification(callback) {
+  getRequest(config.basic.host + config.basic.version + config.api.member.notifications, callback);
 }
 
 /**
- * set warning(red background) badge
+ * 获取当前用户的Board (get Current User Trello Boards)
+ * @param {[object]} [options] [options of the request]
+ * @param  {Function} callback
+ */
+function getBoards(options, callback) {
+  if(callback == null && typeof options == "function") {
+    callback = options; options = null;
+  }
+  getRequest(config.basic.host + config.basic.version + config.api.member.boards, callback);  
+}
+
+/**
+ * 获取指定Board下的List (get lists by board id)
+ * @param  {[string]}   boardId  [board id]
+ * @param  {[object]}   options  [request options]
+ * @param  {Function} callback 
+ */
+function getLists(boardId, options, callback) {
+  if(callback == null && typeof options == "function") {
+    callback = options; options = null;
+  }
+  var url = renderUrl(config.basic.host + config.basic.version + config.api.board.lists, {id: boardId});
+  getRequest(url, callback);
+}
+
+
+// ----------------------------------------------
+
+/**
+ * 设置提醒 (set warning(red background) badge)
  * @param {[string]} text [badge text]
  */
 function setWarningBadge(text) {
@@ -65,17 +116,38 @@ function setWarningBadge(text) {
 }
 
 /**
- * check whether have unread notification
- * @param  {[number]} timer [check interval]
+ * 清除提醒 (clear warning(red background) badge)
  */
-function checkNotifications(timer) {
-  setInterval(function() {
-    getUnreadNoticeNumber(function(number) {
-      if(number > 0) {
-        setWarningBadge(String(number));
-      }
-    })
-  }, timer || 30000);
+function clearWarningBadge() {
+  chrome.browserAction.setBadgeText({text: ""});
 }
 
-checkNotifications();
+/**
+ * 检查是否有未读信息 (check whether have unread notification)
+ * @param  {[number]} timer [check interval]
+ */
+// function checkNotifications(timer) {
+//   setInterval(function() {
+//     getUnreadNoticeNumber(function(number) {
+//       if(number > 0) {
+//         setWarningBadge(String(number));
+//       }
+//     });
+//   }, timer || 30000);
+// }
+
+//checkNotifications();
+
+var oa = oauth(
+  "1f7fc3749300a14a879de2eef7fba060", 
+  "0509b5365debaf38e176068db56e8dca3cc87ce9f29a5e0e4ba107b4ba71df43");
+
+function test() {
+  oa.authorize(function(token,b,c) {
+    console.log(a,b,c);
+    
+    oa.sendSignedRequest("https://api.trello.com/1/members/me", function(res, xhr) {
+      console.log(res);
+    }, {method: "GET"})
+  });
+}
--- assets/javascripts/popup.js
+++ assets/javascripts/popup.js
@@ -1,47 +1,36 @@
 var BG = chrome.extension.getBackgroundPage();
 
 $(function(){
-  $('article').html("<div>Loading...</div>");
+  $("#test").click(function() {
+    BG.test()
+  })
 
-  BG.getCurrentUser(function(result) {
-    var username = result.fullName;
-    var unreadNoticeNum = result.notifications.filter(function(n) {return n.unread;}).length;
-    $('header #user').html(username + '(' + unreadNoticeNum + ')');
+  BG.getBoards(function(boards) {
+    $("article").html(getBoardTemplate(JSON.parse(boards)));
   });
 
-  BG.getBoards(function(result) {
-    var boards = result.boards;
-    $('article').html(getBoardTemplate(boards));
+  BG.getCurrentUser(function(user) {
+    // console.log(JSON.parse(user));
+    // $("header #user").html()
+  });
 
-    $('.board').click(function() {
-      var id = $(this).attr('data-id');
-      BG.getBoardById(id, function(result) {
+  BG.getNotification(function(notifications) {
+    console.log(JSON.parse(notifications));
+  });
 
-        // show lists
-        var lists = result.lists.filter(function(n) {return !n.closed;});
-        $('header #nav').html(getListsTemplate(lists));
-
-        // show cards
-        var cards = result.cards.filter(function(n) {return !n.closed;});
-        $('article').html(getCardsTemplate(cards));
-
-        $(".list").click(function() {
-          // filter card by list tag
-          var id = $(this).attr("data-id");
-          var filterCards = cards.filter(function(n) {return n.idList == id; });
-          $('article').html(getCardsTemplate(filterCards));
-        });
-
-      });
+  $(".board").click(function() {
+    BG.getLists($(this).attr("data-id"), function(lists) {
+      $("article").html(getListsTemplate(JSON.parse(lists)));
     });
   });
+
 });
 
 function getBoardTemplate(boards) {
   var boardTemplate = '';
   for(var i in boards) {
     var background = boards[i].prefs.backgroundColor || 'url(' + boards[i].prefs.backgroundImage + ')';
-    boardTemplate += '<div class="board" style="background:' + background + '" data-id="' 
+    boardTemplate += '<div class="board" style="background:' + background + ';" data-id="' 
     + boards[i].id + '">' + boards[i].name + '</div>';
   }
   return boardTemplate;
